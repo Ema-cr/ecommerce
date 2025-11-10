81faa50b861a6e4291d5a07b22912c3df35283b3
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-11-10 13:56:10 -0500

    add jest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+'use client'
 import ContactForm from '@/components/contactform/ContactForm'
 import React from 'react'
+import Button from '../components/button/Button';
+import Counter from '../components/counter/Counter';
 
 function HomePage() {
+  const handleClick = () => {
+    alert('Button clicked!')
+  }
+
   return (
     <main>
      <div>HomePage</div>
     <ContactForm />
+     <Button label="Click me" onClick={handleClick} />
+     <Counter/>
     </main>
   )
 }
+
 
 export default HomePage
